26ede27085c15d067742150af3cfd98cc0c67646
JaneQuipu <jane.veljanovski@quipu.de>
2021-09-28 15:45:02 +0200

    Mapping #2

--- RcaApiBase/Model/Map/Map.cs
+++ RcaApiBase/Model/Map/Map.cs
@@ -10,6 +10,38 @@ namespace RcaApiBase.Model.Map
 {
     public class Map
     {
+        public Quipu.RcaApiBase.OpenApi.Models.ModelCase MapApplications(RcaApi.Domain.CQRS.CQRSApplication _)
+        {
+            var res = new Quipu.RcaApiBase.OpenApi.Models.ModelCase()
+            {
+                Id = _.Id,
+                Created = _.created,
+                Status = _.Status,
+                Title = _.Title,
+                FieldsData = _.FieldsData,
+                ItemType = _.ItemType,
+                InternalStatus = _.InternalStatus,
+                Owner = _.Owner
+            };
+
+            return res;
+        }
+
+        public Quipu.RcaApiBase.OpenApi.Models.CatalogueItem MapCatalogue(RcaApi.Domain.CQRS.CQRSCatalogue _)
+        {
+            var res = new Quipu.RcaApiBase.OpenApi.Models.CatalogueItem()
+            {
+                Code = _.Code,
+                ParentCode = _.ParentCode,
+                IsFavorite = _.IsFavorite
+            };
+            if (_.ItemType == (int)ItemTypeEnum.ApplicationEnum)
+                res.ItemType = ItemTypeEnum.ApplicationEnum;
+            if (_.ItemType == (int)ItemTypeEnum.ReportEnum)
+                res.ItemType = ItemTypeEnum.ReportEnum;
+
+            return res;
+        }
         public RcaApi.Domain.Applications ModelCaseToApplication(Quipu.RcaApiBase.OpenApi.Models.ModelCase _)
         {
             return new RcaApi.Domain.Applications()
