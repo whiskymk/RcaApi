60a849226a9b69c36384fca38b9c9f6798ebd759
JaneQuipu <jane.veljanovski@quipu.de>
2021-10-25 17:01:09 +0200

    fix for swagger for env.production

--- RcaApi/Startup.cs
+++ RcaApi/Startup.cs
@@ -71,14 +71,14 @@ namespace RcaApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.IsDevelopment() || env.IsProduction())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "RcaApi v1"));
             }
 
-            app.UseHttpsRedirection();
+          //  app.UseHttpsRedirection();
 
             app.UseStaticFiles();
 
